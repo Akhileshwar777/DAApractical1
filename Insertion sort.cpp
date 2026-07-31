520426872154ff433eb0fab4b0daa8129a0e555e
Akhileshwar777 <reddyakhileshwar46@gmail.com>
2026-07-31 15:26:20 +0530

    Insertion sort.cpp

--- Insertion sort.cpp
+++ Insertion sort.cpp
@@ -1,27 +1,26 @@
 #include <iostream>
 #include <vector>
 
-// Function to perform Insertion Sort on a reference vector
+
 void insertionSort(std::vector<int>& arr) {
     int n = arr.size();
     
-    // Iterate from the second element to the end of the array
+   
     for (int i = 1; i < n; ++i) {
         int key = arr[i]; // The element to be positioned
         int j = i - 1;
 
-        // Shift elements of arr[0..i-1] that are greater than key
-        // to one position ahead of their current position
+      
         while (j >= 0 && arr[j] > key) {
             arr[j + 1] = arr[j];
             j = j - 1;
         }
-        // Insert the key into its correct sorted location
+       
         arr[j + 1] = key;
     }
 }
 
-// Utility function to print array elements
+
 void printArray(const std::vector<int>& arr) {
     for (int num : arr) {
         std::cout << num << " ";
